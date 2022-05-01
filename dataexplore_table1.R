5122da13575a7f625d98e5ea79ccc47df2bf3e7a
marybridgetwaters <marybridgetwaters@gmail.com>
2022-05-01 15:28:09 -0700

    initial commit

---
+++ dataexplore_table1.R
@@ -0,0 +1,10 @@
+
+# data exploration & table 1
+# epi 514
+# bridget waters
+# created: may 1 2022
+# last updated: may 1 2022
+
+# description: data exploration, crosstabs, histograms
+  # make table 1 using table1 package
+
